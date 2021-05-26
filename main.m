bbae2ed69f3ddd20afb46e54b7d354094b7c8f3e
Chandralinjo <boris.majic@gmail.com>
2021-05-26 21:15:41 +0200

    Initial script
    Just load file and detect person on the first frame

---
+++ main.m
@@ -0,0 +1,27 @@
+% Ova skripta treba da demonstrira ceo flow, od u?itavanja fajla, preko
+% detekcije osobe do prepoznavanja pokreta
+
+%% U?itavanje fajla
+file_path = '/Users/boris_majic/Downloads/trainning2/Sample00152/Sample00152_color.mp4';
+file2 = '/Users/boris_majic/Downloads/trainning2/Sample00101/Sample00101_color.mp4';
+
+% Pravimo VideoReader objekat koji ?e iterirati kroz frejmove
+v = VideoReader(file2);
+
+%% Detekcija ?oveka
+% Detekciju ?oveka radimo na prvom frejmu. Pretpostavka je da je osoba
+% prisutna u prvom frejmu.
+
+frame = readFrame(v);
+imshow(frame)
+
+% figure()
+% frame = imresize(frame, 0.5);
+% imshow(frame)
+
+% Za detekciju koristimo detectPeopleACF funkciju
+[bboxes,scores] = detectPeopleACF(frame);
+
+I = insertObjectAnnotation(frame,'rectangle',bboxes,scores);
+figure, imshow(I)
+title('Detektovane osobe');
